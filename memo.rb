f531ae80c50a7d5c7a072ffa0ea54fc715fa9c9b
nekonote <nekonote.civ@gmail.com>
2023-02-25 10:42:19 +0000

    個別メモページの表示処理作成

--- memo.rb
+++ memo.rb
@@ -5,18 +5,34 @@ require 'json'
 
 BASE_STATIC_PATH = './public/'
 
-def read_json_contents
+def read_files
   base_path = "#{BASE_STATIC_PATH}/json/"
-  files = Dir.glob('*.json', base: base_path)
+  Dir.glob('*.json', base: base_path)
+end
+
+def read_all_json_contents
+  files = read_files
   files.map do |file|
     full_path = "#{base_path}#{file}"
     File.open(full_path) { |open_file| JSON.load(open_file) }
   end
 end
 
+def read_json_contents(memo_id)
+  files = read_files
+  files.each do |file|
+    full_path = "#{base_path}#{file}"
+    result_file = File.open(full_path) do |open_file|
+      json = JSON.load(open_file)
+      json if json['id'] == memo_id
+    end
+    return result_file if result_file
+  end
+end
+
 # メモ一覧画面
 get '/memos' do
-  @contents = read_json_contents
+  @contents = read_all_json_contents
   erb :memos
 end
 
@@ -27,7 +43,8 @@ end
 
 # メモ表示画面
 get '/memos/:memo_id' do
-  erb :show
+  @contents = read_json_contents(params['memo_id'])
+  erb :memo
 end
 
 # メモ編集画面
